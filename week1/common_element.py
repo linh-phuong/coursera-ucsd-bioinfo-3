09416908ecf5bf49603731f3fce9362eb8a348cf
LPhuong Nguyen <phuong.nguyen@upmc.fr>
2021-01-31 00:39:42 +0100

    find longest route

--- week1/common_element.py
+++ week1/common_element.py
@@ -1,4 +1,8 @@
-def find_longest_common(a, b):
+import numpy as np
+from collections import defaultdict
+
+
+def find_longest_common(a, b):  # This function is not in the excercise
     """find the longest common string between a and b (duplicates and position included)
 
     Args:
@@ -88,3 +92,153 @@ def find_min_elements(S, elements):
         if i > max_e:
             del min_dict[i - max_e - 1]
     return min_dict[S]
+
+
+def find_longest_route_value_MT(c, r, D, R):
+    n = c + 1
+    m = r + 1
+    route_score = np.zeros((n, m))
+    for i in range(1, n):
+        route_score[i, 0] = route_score[i - 1, 0] + D[i - 1, 0]
+    for j in range(1, m):
+        route_score[0, j] = route_score[0, j - 1] + R[0, j - 1]
+    for i in range(1, n):
+        for j in range(1, m):
+            route_score[i, j] = max(
+                (route_score[i - 1, j] + D[i - 1, j], route_score[i, j - 1] + R[i, j - 1])
+            )
+    return route_score[n - 1, m - 1]
+
+
+def find_longest_route_value_MTD(c, r, D, R, DG):
+    n = c + 1
+    m = r + 1
+    route_score = np.zeros((n, m))
+    for i in range(1, n):
+        route_score[i, 0] = route_score[i - 1, 0] + D[i - 1, 0]
+    for j in range(1, m):
+        route_score[0, j] = route_score[0, j - 1] + R[0, j - 1]
+    for i in range(1, n):
+        for j in range(1, m):
+            route_score[i, j] = max(
+                (
+                    route_score[i - 1, j] + D[i - 1, j],
+                    route_score[i, j - 1] + R[i, j - 1],
+                    route_score[i - 1, j - 1] + DG[i - 1, j - 1],
+                )
+            )
+    return route_score[n - 1, m - 1]
+
+
+def construct_route(v, w):
+    scan_v = len(v) + 1
+    scan_w = len(w) + 1
+    align = np.zeros((scan_v, scan_w))
+    backtrack = [["s"] * scan_w]
+    for i in range(1, scan_v):
+        tr = ["s"]
+        for j in range(1, scan_w):
+            match = 1 if v[i - 1] == w[j - 1] else 0
+            align[i, j] = max(align[i - 1, j], align[i, j - 1], align[i - 1, j - 1] + match)
+            if align[i, j] == align[i - 1, j]:
+                tr.append("d")
+            elif align[i, j] == align[i, j - 1]:
+                tr.append("r")
+            elif align[i, j] == align[i - 1, j - 1] + match:
+                tr.append("dg")
+        backtrack.append(tr)
+    return backtrack
+
+
+def _track_longest_route(tracks, v, i, j):
+    if i == 0 or j == 0:
+        return ""
+    if tracks[i][j] == "d":
+        return _track_longest_route(tracks, v, i - 1, j)
+    elif tracks[i][j] == "r":
+        return _track_longest_route(tracks, v, i, j - 1)
+    else:
+        return _track_longest_route(tracks, v, i - 1, j - 1) + v[i - 1]
+
+
+def track_longest_route(v, w):
+    tracks = construct_route(v, w)
+    i = len(v)
+    j = len(w)
+    return _track_longest_route(tracks, v, i, j)
+
+
+def select_route_with_startend(routes_dict, start, end):
+    bestscore = 0
+    bestroute = ()
+    for s, p in routes_dict.values():
+        if p[0] == start and p[-1] == end and s > bestscore:
+            bestscore = s
+            bestroute = p
+    return bestscore, bestroute
+
+
+# graph: { 0: [(1, 3)] } => arc from 0 to 1 has weight 3
+def find_highest_score_track(graph, start, end):
+
+    # backtrack: { 1: [(0, 3)] } => arc from 0 to 1 has weight 3
+    backtrack = convert_to_backtrack(graph)
+
+    allnodes = set(list(backtrack.keys()) + [n[0] for v in backtrack.values() for n in v])
+    allnodes = sorted(allnodes)
+    trimed_nodes = [i for i in allnodes if end >= i > start]
+
+    # { 5: (7, [0, 3, 5])} => route from 0 to 5 with the highest score is 0-3-5 with score 7
+    routes_dict = {start: (0, [start])}
+
+    for i in trimed_nodes:
+        bestroute, bestscore = None, -1
+        for n, sc in backtrack[i]:  # edge from n to i has score sc
+            if n in routes_dict:
+                prev_score, prev_route = routes_dict[n]  # score and route from start to n
+                score = sc + prev_score
+                if score > bestscore:
+                    bestroute, bestscore = prev_route + [i], score
+        if bestroute is not None:
+            routes_dict[i] = (bestscore, bestroute)
+    if end not in routes_dict:
+        raise Exception("The end node is not connected to the start node")
+    return routes_dict[end]
+
+
+def max_with_tie(ls, id):
+    m_score = 0
+    item = []
+    for it in ls:
+        if it[id] > m_score:
+            item = [it]
+            m_score = it[id]
+        elif it[id] == m_score:
+            item.append(it)
+    return item
+
+
+def max_with_start_end(ls, start, end):
+    m_score = 0
+    item = []
+    ls = sorted(ls)
+    critical_node = False
+    for it in ls:
+        if it[0] == start or it[0] == end:
+            item = [it]
+            critical_node = True
+        if not critical_node:
+            if it[1] > m_score:
+                item = [it]
+                m_score = it[1]
+            elif it[1] == m_score:
+                item.append(it)
+    return item
+
+
+def convert_to_backtrack(graph):
+    backtrack = defaultdict(lambda: [])
+    for items in graph:
+        backtrack[items[1]].append((items[0], items[2]))
+    return backtrack
+
--- week1/test_common_element.py
+++ week1/test_common_element.py
@@ -1,4 +1,25 @@
-from week1.common_element import find_longest_common, find_min_elements, min_nb_elements
+from week1.common_element import (
+    convert_to_backtrack,
+    find_highest_score_track,
+    find_longest_common,
+    find_longest_route_value_MT,
+    find_longest_route_value_MTD,
+    find_min_elements,
+    max_with_start_end,
+    max_with_tie,
+    min_nb_elements,
+    construct_route,
+    select_route_with_startend,
+    track_longest_route,
+)
+import numpy as np
+from pathlib import Path
+import pytest
+import textwrap
+import sys
+import re
+
+sys.setrecursionlimit(1500)
 
 
 def test_find_longest_common():
@@ -27,3 +48,297 @@ def test_find_min_elements():
     assert find_min_elements(2, c) == [1, 1]
     assert find_min_elements(4, c) == [4]
     assert find_min_elements(22, c) == [5, 5, 4, 4, 4]
+
+
+def test_find_longest_route():
+    D = np.array([[1, 1, 1], [1, 1, 1]])
+    R = np.array([[1, 1], [1, 1], [1, 1]])
+    assert find_longest_route_value_MT(2, 2, D, R) == 4
+    D = np.array([[1, 1, 1], [1, 1, 1]])
+    R = np.array([[4, 1], [1, 1], [1, 1]])
+    assert find_longest_route_value_MT(2, 2, D, R) == 7
+    D = np.array([[1, 0, 2, 4, 3], [4, 6, 5, 2, 1], [4, 4, 5, 2, 1], [5, 6, 8, 5, 3]])
+    R = np.array([[3, 2, 4, 0], [3, 2, 4, 2], [0, 7, 3, 3], [3, 3, 0, 2], [1, 3, 2, 2]])
+    assert find_longest_route_value_MT(4, 4, D, R) == 34
+
+
+def _parse_longest_route_test(file):
+    with open(file) as fd:
+        L = fd.readline().strip().split()
+        n = int(L[0].strip())
+        m = int(L[1].strip())
+        D = []
+        R = []
+        while True:
+            L = fd.readline().strip().split()
+            if L[0] == "-":
+                break
+            D.append([int(i.strip()) for i in L])
+        while True:
+            L = fd.readline().strip().split()
+            if not L:
+                break
+            R.append([int(i.strip()) for i in L])
+        return n, m, np.array(D), np.array(R)
+
+
+def _parse_longest_route_large():
+    with open("week1/data/Manhattan_tourist.txt") as fd:
+        fd.readline()
+        L = fd.readline().strip().split()
+        n = int(L[0].strip())
+        m = int(L[1].strip())
+        D = []
+        R = []
+        while True:
+            L = fd.readline().strip().split()
+            if L[0] == "-":
+                break
+            D.append([int(i.strip()) for i in L])
+        while True:
+            L = fd.readline().strip().split()
+            if not L:
+                break
+            R.append([int(i.strip()) for i in L])
+        fd.readline()
+        r_exp = int(fd.readline().strip())
+        return n, m, np.array(D), np.array(R), r_exp
+
+
+def test_parse_longest_route(tmp_path):
+    fn = tmp_path / "a.txt"
+    fn.write_text(
+        textwrap.dedent(
+            """
+    3 3
+    0 1 2
+    3 4 5
+    -
+    6 7
+    8 9
+    10 11
+    """
+        ).strip()
+    )
+    n, m, D, R = _parse_longest_route_test(fn)
+    assert n == 3
+    assert m == 3
+    assert (D == np.array([[0, 1, 2], [3, 4, 5]])).all()
+    assert (R == np.array([[6, 7], [8, 9], [10, 11]])).all()
+
+
+TEST_DIR = Path("week1/data/LongestPathGrid/inputs/")
+
+
+@pytest.mark.parametrize("inp", sorted(TEST_DIR.glob("*")))
+def test_longest_route(inp):
+    out = str(inp).replace("/inputs/", "/outputs/")
+    n, m, D, R = _parse_longest_route_test(inp)
+    with open(out) as fd:
+        r_exp = int(fd.readline().strip())
+    r = find_longest_route_value_MT(n, m, D, R)
+    assert r == r_exp
+
+
+def test_longest_route_large():
+    n, m, D, R, r_exp = _parse_longest_route_large()
+    r = find_longest_route_value_MT(n, m, D, R)
+    assert r == r_exp
+
+
+def test_longest_route_value_with_diagonal():
+    D = np.array([[1, 1, 1], [1, 1, 1]])
+    R = np.array([[1, 1], [1, 1], [1, 1]])
+    DG = np.array([[1, 1], [1, 1]])
+    assert find_longest_route_value_MTD(2, 2, D, R, DG) == 4
+    D = np.array([[1, 1, 1], [1, 1, 1]])
+    R = np.array([[4, 1], [1, 1], [1, 1]])
+    DG = np.array([[1, 1], [1, 10]])
+    assert find_longest_route_value_MTD(2, 2, D, R, DG) == 15
+
+
+def test_construct():
+    assert construct_route("AACC", "ACAC") == [
+        ["s", "s", "s", "s", "s"],
+        ["s", "dg", "r", "r", "r"],
+        ["s", "d", "d", "dg", "r"],
+        ["s", "d", "dg", "d", "dg"],
+        ["s", "d", "d", "d", "d"],
+    ]
+
+
+def test_track_route():
+    v = "AACC"
+    w = "ACAC"
+    assert track_longest_route(v, w) == "AAC"
+    v = "AACCTTGG"
+    w = "ACACTGTGA"
+    assert track_longest_route(v, w) == "AACTTG"
+
+
+def _parse_longest_common(file):
+    with open(file) as fd:
+        v = fd.readline().strip()
+        w = fd.readline().strip()
+    return v, w
+
+
+TEST_DIR = Path("week1/data/LongestCommonSubsequence/inputs")
+
+
+@pytest.mark.parametrize("inp", sorted(TEST_DIR.glob("*")))
+def test_common_route(inp):
+    out = str(inp).replace("/inputs/", "/outputs/")
+    v, w = _parse_longest_common(inp)
+    with open(out) as fd:
+        cm_exp = fd.readline().strip()
+    cm = track_longest_route(v, w)
+    assert cm == cm_exp
+
+
+def _parse_common_large():
+    with open("week1/data/longest_common_subsequence.txt") as fd:
+        fd.readline()
+        v = fd.readline().strip()
+        w = fd.readline().strip()
+        fd.readline()
+        cm = fd.readline().strip()
+    return v, w, cm
+
+
+def test_longest_common_large():
+    v, w, cm_exp = _parse_common_large()
+    cm = track_longest_route(v, w)
+    assert cm == cm_exp
+
+
+def test_convert_to_backtrack():
+    graph = ((0, 1, 7), (1, 4, 1), (3, 4, 3))
+    assert convert_to_backtrack(graph) == {1: [(0, 7)], 4: [(1, 1), (3, 3)]}
+
+
+def test_find_backtrack():
+    graph = ((0, 1, 7), (0, 2, 4), (2, 3, 2), (1, 4, 1), (3, 4, 3))
+    assert find_highest_score_track(graph, 0, 4) == (9, [0, 2, 3, 4])
+
+
+def _parse_backtrack_inp(file):
+    with open(file) as fd:
+        start = int(fd.readline().strip())
+        end = int(fd.readline().strip())
+        graph = []
+        while True:
+            line = fd.readline().strip()
+            if not line:
+                break
+            line = re.split("->|:", line)
+            graph.append([int(i.strip()) for i in line])
+    return start, end, graph
+
+
+def _parse_backtrack_out(file):
+    with open(file) as fd:
+        score = int(fd.readline().strip())
+        L = fd.readline().strip().split("->")
+        track = [int(i.strip()) for i in L]
+    return score, track
+
+
+def test_parse_backtrack(tmp_path):
+    fn = tmp_path / "a.txt"
+    fn.write_text(
+        textwrap.dedent(
+            """
+    0
+    10
+    0->1:2
+    3->4:5
+    """
+        ).strip()
+    )
+    st, end, gr = _parse_backtrack_inp(fn)
+    assert st == 0
+    assert end == 10
+    assert gr == [[0, 1, 2], [3, 4, 5]]
+
+
+TEST_DIR = Path("week1/data/LongestPathDAG/inputs/")
+
+
+@pytest.mark.parametrize("inp", sorted(TEST_DIR.glob("*")))
+def test_backtrack(inp):
+    out = str(inp).replace("/inputs/", "/outputs/")
+    start, end, graph = _parse_backtrack_inp(inp)
+    score_exp, track_exp = _parse_backtrack_out(out)
+    score, track = find_highest_score_track(graph, start, end)
+    assert score == score_exp
+    assert track == track_exp
+
+
+def test_max_with_tie():
+    assert max_with_tie(((1, 3), (2, 3)), 1) == [(1, 3), (2, 3)]
+
+
+def _parse_longest_route_DAG_large(file):
+    with open(file) as fd:
+        fd.readline()
+        start = int(fd.readline().strip())
+        end = int(fd.readline().strip())
+        graph = []
+        while True:
+            L = fd.readline().strip()
+            if L == "":
+                break
+            L = re.split("->|:", L)
+            graph.append([int(i.strip()) for i in L])
+        fd.readline()
+        score = int(fd.readline().strip())
+        results = fd.readline().strip().split("->")
+        path = [int(i.strip()) for i in results]
+    return start, end, graph, score, path
+
+
+def test_find_longest_route_DAG_large():
+    start, end, graph, score, path = _parse_longest_route_DAG_large(
+        "week1/data/longest_path_in_DAG.txt"
+    )
+    sc, pa = find_highest_score_track(graph, start, end)
+    assert sc == score
+    assert pa == path
+
+
+def test_parse_longest_route_large(tmp_path):
+    fn = tmp_path / "a.txt"
+    fn.write_text(
+        textwrap.dedent(
+            """
+    Input:
+    0
+    10
+    0->1:2
+    3->4:5
+
+    Output:
+    10
+    0->1->2
+    """
+        ).strip()
+    )
+    st, end, gr, sc, path = _parse_longest_route_DAG_large(fn)
+    assert st == 0
+    assert end == 10
+    assert gr == [[0, 1, 2], [3, 4, 5]]
+    assert sc == 10
+    assert path == [0, 1, 2]
+
+
+def test_select_route_with_se():
+    route = {0: (3, [0, 1, 2]), 1: (4, [1, 5])}
+    assert select_route_with_startend(route, 0, 2) == (3, [0, 1, 2])
+    assert select_route_with_startend(route, 1, 5) == (4, [1, 5])
+
+
+def test_max_with_start_end():
+    ls = [(0, 3), (1, 4), (2, 6)]
+    assert max_with_start_end(ls, 0, 5) == [(0, 3)]
+    assert max_with_start_end(ls, 1, 10) == [(1, 4)]
